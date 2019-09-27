362fa89bee3aff643ad178add6372906eea273da
Alex Quiñones <alexquinones@monobits.mx>
2019-09-26 20:34:42 -0700

    Add project files.

---
+++ Restaurante.Core/Entities/Restaurante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurante.Core.Entities
+{
+    public class Restaurante
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Domicilio { get; set; }
+        public int Telefono { get; set; }
+        public string Logo { get; set; }
+        public string PaginaWeb { get; set; }
+        public DateTime FechaDeAlta { get; set; }
+        public int? HoraDeCierre { get; set; }
+    }
+}
---
+++ Restaurante.Infrastructure/Data/AppDbContextSeed.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Restaurante.Infrastructure.Data
+{
+    public class AppDbContextSeed
+    {
+        public static void Seed(AppDbContext catalogContext)
+        {
+            if (!catalogContext.Restaurantes.Any())
+            {
+                catalogContext.Add(new Restaurante.Core.Entities.Restaurante
+                {
+                    Nombre = "Restaurante Maestro",
+                    Domicilio = "Ave. Prueba 123",
+                    Telefono = 686156
+                });
+
+
+                catalogContext.SaveChanges();
+            }
+        }
+    }
+}
