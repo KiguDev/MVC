8a57200927d248246e57fba34d748e07bc048bba
Carlos Rondero <chatogol_97@outlook.com>
2019-10-11 19:25:08 -0700

    arreglar total, empezar con el post de ordenes y demostrar lista de ordenes.

---
+++ Restaurantes/Models/OrdenRestauranteViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurantes.Models
+{
+    public class OrdenRestauranteViewModel
+    {
+        public int OrdenId { get; set; }
+        public int RestauranteId { get; set; }
+    }
+}
